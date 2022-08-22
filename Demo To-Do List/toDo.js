1740516928ec735d9c1bf3b28bfbb10508d97800
megha739 <megha.nigam_cs19@gla.ac.in>
2022-08-22 16:28:59 +0530

    creating todo list

--- Demo To-Do List/toDo.js
+++ Demo To-Do List/toDo.js
@@ -1,1 +1,46 @@
 //selector
+var todoinput=document.querySelector(".todo-input");
+var btn=document.querySelector("button");
+var todolist=document.querySelector(".todo-list");
+
+btn.onclick=function createTodo(e){
+    e.preventDefault();
+    var newDiv=document.createElement("div");
+    newDiv.classList.add("todo");
+
+    var newli=document.createElement("li");
+    newli.classList.add("todo_item");
+    newli.innerHTML=todoinput.Value;
+    newDiv.appendChild(newli);
+
+    var cmpltbtn=document.createElement("button");
+    cmpltbtn.classList.add("cmpltbtn");
+    cmpltbtn.innerHTML='<i class="fa fa-check"></i>';
+    newDiv.appendChild(cmpltbtn);
+
+    var deltbtn=document.createElement("button");
+    deltbtn.classList.add("deltbtn");
+    deltbtn.innerHTML='<i class="fa fa-trash"></i>';
+    newDiv.appendChild(deltbtn);
+    //todolist.appendChild(newDiv);
+
+    todolist.appendChild(newDiv);
+    todoinput.Value=" ";
+
+
+};
+todolist.onclick=function checkBtn(e){
+    var check=e.target;
+
+    if(check.classList[0]=='deltbtn'){
+        var parentNode=check.parentElement;
+        parentNode.remove();
+
+    }
+    else if(check.classList[0]=='cmpltbtn'){
+        var parentNode=check.parentElement;
+        parentNode.classList.add("check");
+
+
+    }
+}
