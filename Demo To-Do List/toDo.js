85be49a2d4e489c66a0e4e31cc5591ddce81fc9e
megha739 <megha.nigam_cs19@gla.ac.in>
2022-08-23 12:13:08 +0530

    adding local storage functionality

--- Demo To-Do List/toDo.js
+++ Demo To-Do List/toDo.js
@@ -1,48 +1,152 @@
-//selector
-var todoInput=document.querySelector(".todo-input");
-var btn=document.querySelector("button");
-var todolist=document.querySelector(".todo-list");
+var toInput=document.querySelector(".todo-input");
+var btn = document.querySelector("button");
+var todoList=document.querySelector(".todo-list");
+var ll=0;
+ll=Number(window.localStorage.getItem('numberItems'));
+console.log(ll);
 
-btn.onclick=function createTodo(e){
-    e.preventDefault();
+
+for(var i=0;i<50;i++)
+{
+    var rr="tt"+i;
+    if(window.localStorage.getItem(rr)==null)
+    {
+            break;
+    }
+    else
+    {
+    
     var newDiv=document.createElement("div");
-    newDiv.classList.add("todo");
+    newDiv.classList.add('todo');
 
-    var newli=document.createElement("li");
-    newli.classList.add("todo_items");
-    newli.innerHTML=todoInput.value
-    //newli.innerHTML=todoInput.Value;
-    newDiv.appendChild(newli);
+    var newLi=document.createElement("li");
+    newLi.classList.add("todo_item");
+    //newLi.innerHTML=toInput.value ;
+    //console.log(oo);
+     newLi.innerHTML=window.localStorage.getItem(rr) ;
+
+    // document.getElementById("stored").innerHTML= window.localStorage.getItem(oo);
+    newDiv.appendChild(newLi);
+
 
     var cmpltbtn=document.createElement("button");
     cmpltbtn.classList.add("cmpltbtn");
     cmpltbtn.innerHTML='<i class="fa fa-check"></i>';
     newDiv.appendChild(cmpltbtn);
 
+
     var deltbtn=document.createElement("button");
     deltbtn.classList.add("deltbtn");
     deltbtn.innerHTML='<i class="fa fa-trash"></i>';
     newDiv.appendChild(deltbtn);
-    //todolist.appendChild(newDiv);
+
+
+    todoList.appendChild(newDiv);
+    }
+
+}
+
+todoList.onclick=function checkbtn(e){
+    var check = e.target;     //e.target btata hai kis cheez ko target kiya hua hai 
+    // console.log(check);
+
+    if(check.classList[0] == "deltbtn"){
+        // console.log("Delete button pressed");
+        var parentNode=check.parentElement;
+        // console.log(parentNode);
+        parentNode.remove();
+    }
+    else if(check.classList[0] == "cmpltbtn"){
+        // console.log("Complete button pressed");
+        var parentNode=check.parentElement;
+        parentNode.classList.add("check")
+    }
+};
+
+
+
+
+
+
+    btn.onclick = function createToDo(e){
+    e.preventDefault();
+
+    if(toInput.value=="")
+    {
+        alert("write something to add");
+       
+    }
+    else
+    {
+
+        
+       var oo="tt"+ Number(ll);
+
+        window.localStorage.setItem(oo,toInput.value);
+        console.log(oo);
+        ll=Number(ll)+1;
+        //console.log(ll);
+        window.localStorage.setItem("numberItems",ll);
+  
+
 
     
-    todoInput.value= "";
-    todolist.appendChild(newDiv);
+
+    var newDiv=document.createElement("div");
+    newDiv.classList.add('todo');
+
+    var newLi=document.createElement("li");
+    newLi.classList.add("todo_item");
+    //newLi.innerHTML=toInput.value ;
+    //console.log(oo);
+     newLi.innerHTML=window.localStorage.getItem(oo) ;
+
+    // document.getElementById("stored").innerHTML= window.localStorage.getItem(oo);
+    newDiv.appendChild(newLi);
+
+
+    var cmpltbtn=document.createElement("button");
+    cmpltbtn.classList.add("cmpltbtn");
+    cmpltbtn.innerHTML='<i class="fa fa-check"></i>';
+    newDiv.appendChild(cmpltbtn);
+
+
+    var deltbtn=document.createElement("button");
+    deltbtn.classList.add("deltbtn");
+    deltbtn.innerHTML='<i class="fa fa-trash"></i>';
+    newDiv.appendChild(deltbtn);
+
+
+    todoList.appendChild(newDiv);
+
+    
+
+    // var tododatastore={
+    //             listdata: toInput.value
+    // }
+    // window.localStorage.setItem("todoDta",JSON.stringify(tododatastore));
+
+    toInput.value="";
 
 };
-todolist.onclick=function checkBtn(e){
-    var check=e.target;
-    console.log(check);
-
-    if(check.classList[0]=='deltbtn'){
-        var parentNode=check.parentElement;
-        parentNode.remove();
-
-    }
-    else if(check.classList[0]=='cmpltbtn'){
-        var parent=check.parentElement;
-        parent.classList.add("check");
 
 
-    }
+    todoList.onclick=function checkbtn(e){
+        var check = e.target;     //e.target btata hai kis cheez ko target kiya hua hai 
+        // console.log(check);
+
+        if(check.classList[0] == "deltbtn"){
+            // console.log("Delete button pressed");
+            var parentNode=check.parentElement;
+            // console.log(parentNode);
+            parentNode.remove();
+        }
+        else if(check.classList[0] == "cmpltbtn"){
+            // console.log("Complete button pressed");
+            var parentNode=check.parentElement;
+            parentNode.classList.add("check")
+        }
+    };
 }
+
+
